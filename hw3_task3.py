231246f9f66d51ce857c1b8e80cfe0ac1074436e
Ulyana <ulyana_yezhgurova@mail.ru>
2023-07-25 15:59:35 +0300

    hw3 task3

--- hw3_task3.py
+++ hw3_task3.py
@@ -73,29 +73,6 @@
 # print(points)
 
 
-
-# import re
-# def Scrabble(text):
-# 	return bool(re.search("[а-яА-Я]", text))
-# Rus = { 1:"А, В, Е, И, Н, О, Р, С, Т",
-#       	2:"Д, К, Л, М, П, У",
-#         3:"Б, Г, Ё, Ь, Я",
-#         4:"Й, Ы",
-#         5:"Ж, З, Х, Ц, Ч",
-#         8:"Ш, Э, Ю",
-#         10:"Ф, Щ, Ъ"}
-# Eng = { 1:"A, E, I, O, U, L, N, S, T, R ",
-#         2:"D, G",
-#         3:"B, C, M, P",
-#         4:"F, H, V, W, Y",
-#         5:"K",
-#         8:"J, X"}
-# text = input("Введите слово: ").upper()
-# if Scrabble(text):
-# 	print(sum([k for i in text for k, v in Rus.items() if i in v]))
-# else:
-# 	print(sum([k for i in text for k, v in Eng.items() if i in v]))
-
 k = 'ноутбук'
 
 dict = {1:"AEIOULNSTRАВЕИНОРСТ",
