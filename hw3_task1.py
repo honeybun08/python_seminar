7f43c2dba8933cd850bd6cec57fa731fb3d32dd9
Ulyana <ulyana_yezhgurova@mail.ru>
2023-07-25 15:50:49 +0300

    sem3 task1

---
+++ hw3_task1.py
@@ -0,0 +1,46 @@
+#Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+#Найдите количество и выведите его.
+
+#Пример:
+#list_1 = [1, 2, 3, 4, 5]
+#k = 3
+# 1
+
+# # 1й вариант
+# list_1 = []
+
+# while True:
+#     n = int(input('Введите длину списка -> '))
+#     if n > 0:
+#         j = 1
+#         while j <= n:
+#             list_1.append(int(input(f'Введите {j} элемент массива -> ')))
+#             j += 1
+#         break
+
+# print(list_1)
+
+
+# k = (int(input('Введите число k:')))
+
+# count_k = 0
+
+# for i in list_1:
+#     if i == k:
+#         count_k +=1
+#     i +=1
+
+# print(count_k)
+
+# 2й вариант
+list_1 = [1,2,3,4,4,5,5,7,2,6]
+k = 1
+
+count_k = 0
+
+for i in list_1:
+    if i == k:
+        count_k +=1
+    i +=1
+
+print(count_k)
